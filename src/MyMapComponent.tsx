126885db4cc331b92cdf0de3865b4a3335d179c7
vdedios <victor.dedios@outlook.com>
2022-10-22 13:52:34 +0200

    revert map in App.tsx

--- src/MyMapComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-
-const MyMapComponent = () => {
-    const map = new google.maps.Map(()=> {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
-    return <div id="map" style={{ width: "100%", height: "100%" }} />;
-};
